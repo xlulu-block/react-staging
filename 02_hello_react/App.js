911f691258994d67afce51b03fb83d6877a10ce0
suwenlong <958089904@qq.com>
2023-01-29 17:08:11 +0800

    fix:改造目录

--- 02_hello_react/App.js
+++ 02_hello_react/App.js
@@ -1,9 +1,12 @@
 import React,{Component} from "react";
-
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 // 创建并暴露App组件
 export default class App extends Component{
     render(){
         return (<div>
+                <Hello/>
+                <Welcome/>
         </div>)
     }
 }
