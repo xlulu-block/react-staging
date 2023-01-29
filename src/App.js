fca983f0eccb546c5b56cd83b1184a2749df514c
suwenlong <958089904@qq.com>
2023-01-29 17:36:23 +0800

    feat:todolist静态组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React,{Component} from "react";
-
+import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import List from "./components/List";
 // 创建并暴露App组件
 export default class App extends Component{
     render(){
-        return (<div>
-        </div>)
+        return (<div className="todo-container">
+                    <div className="todo-wrap">
+                    <Header/>
+                    <List/>
+                    <Footer/>
+                    </div>
+         </div>)
     }
 }
---
+++ src/components/List/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.css'
+import Item from '../Item'
+export default class Header extends Component {
+    render() {
+        return ( <ul className="todo-main">
+       <Item/>
+    </ul>
+        )
+    }
+}
