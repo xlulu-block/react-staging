a7c7abcda65faf4a14ac068b68682721a8684a23
suwenlong <958089904@qq.com>
2023-01-30 20:14:29 +0800

    feat:部分footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import List from "./components/List";
 // 创建并暴露App组件
 export default class App extends Component{
     state={
-        todo:[{id:'001',name:'吃饭',done:true},{id:'002',name:'睡觉',done:false},{id:'003',name:'敲代码',done:true}]
+        todo:[{id:'001',name:'吃饭',done:false},{id:'002',name:'睡觉',done:false},{id:'003',name:'敲代码',done:false}],
     }
     // 新增列表数据
     changeListData=(data)=>{
@@ -15,11 +15,13 @@ export default class App extends Component{
     }
     // 复选框状态发生变化
     changeChecked=(data)=>{
-        console.log(data);
-        const {todo}=this.state
+        const {todo,}=this.state
         const dataValue=todo[todo.findIndex(item=>item.id===data)].done
         todo[todo.findIndex(item=>item.id===data)].done=!dataValue
         this.setState({todo:todo})
+        // 筛选选中的数据
+        const newDoneList=todo.filter(item=>item.done===true)
+        this.setState({doneList:newDoneList})
     }
     // 删除todo数据
     deleteTodo=(data)=>{
@@ -30,13 +32,20 @@ export default class App extends Component{
             this.setState({todo:todo})
         }
     }
+    // 全选数据
+    checkedAll=(flag)=>{
+        const {todo}=this.state
+        const newTodo=todo.map((item)=>item.done=flag)
+        this.setState({todo:newTodo})
+        
+    }
     render(){
         const {todo}=this.state
         return (<div className="todo-container">
                     <div className="todo-wrap">
                         <Header changeListData={this.changeListData}/>
                         <List todo={todo} changeChecked={this.changeChecked} deleteTodo={this.deleteTodo} />
-                        <Footer/>
+                        <Footer todo={todo} checkedAll={this.checkedAll} />
                     </div>
          </div>)
     }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,16 +2,34 @@ import React, { Component } from 'react'
 import './index.css'
 // import PropTypes from 'prop-types'
 export default class Header extends Component {
+    state={
+        allChecked:false
+    }
+    handleCheckedAll=()=>{
+        return (event)=>{
+            this.props.checkedAll(event.target.checked)
+        }
+    }
+    // 反选全选
+    judgeIsAllChecked=(doneCount,todo)=>{
+        if(doneCount===todo.length){
+            this.setState({allChecked:true})
+        }
+    }
     render() {
+        const {todo}=this.props
+        const {allChecked}=this.state
+        const doneCount=todo.reduce((pre,cru)=>pre+(cru.done?1:0),0)
+        this.judgeIsAllChecked(doneCount,todo)
         return (  <div className="todo-footer">
-        <label>
-        <input type="checkbox"/>
-        </label>
-        <span>
-        <span>已完成0</span> / 全部2
-        </span>
-        <button className="btn btn-danger">清除已完成任务</button>
-    </div>
+                    <label>
+                    <input type="checkbox" onChange={this.handleCheckedAll} defaultChecked={allChecked}/>
+                    </label>
+                    <span>
+                    <span>已完成{doneCount}</span> / 全部{todo.length}
+                    </span>
+                    <button className="btn btn-danger">清除已完成任务</button>
+                </div>
         )
     }
 }
