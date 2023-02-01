5478a82bfa130d9a69f546de84622d3d702c730c
swl <958089904@qq.com>
2023-02-01 23:53:02 +0800

    feat:二级路由

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 import './index.css'
+import MyNavLink from '../../components/MyNavLink'  //对navLink进行的二次封装
+import Messages from './Messages'
+import News from './News'
+import { Route, Redirect,Switch } from 'react-router-dom'
 export default class index extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <div>
+        <ul class="nav nav-tabs">
+          <li>
+            <MyNavLink to='/home/news'>News</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to='/home/message'>Msg</MyNavLink>
+          </li>
+        </ul>
+        {/* 路径和component一一匹配,提高效率 */}
+        <Switch>
+          <Route path='/home/message' component={Messages} />
+          <Route path='/home/news' component={News} />
+          {/* 路由重定向 */}
+          <Redirect to='/home/news' />
+        </Switch>
+
+
+      </div>
+
     )
   }
 }
