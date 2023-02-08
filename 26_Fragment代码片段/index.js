fa43a587e20bd74ea2e80f9a28b4a95cbd0acc27
suwenlong <958089904@qq.com>
2023-02-08 14:42:58 +0800

    feat:Fragment代码片段

---
+++ 26_Fragment代码片段/index.js
@@ -0,0 +1,15 @@
+// 引入React核心库
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+import {BrowserRouter} from 'react-router-dom'
+const root =ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
+// store中的函数，只要state发生变化，就会执行，react-redux不需要去监测实时更新
+// store.subscribe(()=>{
+//     root.render(<App/>) 
+// })
