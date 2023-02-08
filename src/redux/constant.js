a58ffd523acc6c21272074b1abe8492c40164f05
suwenlong <958089904@qq.com>
2023-02-08 10:12:18 +0800

    feat:setState的两种更新方式

--- src/redux/constant.js
+++
@@ -1,4 +0,0 @@
-// 命名action和reducer中的常量文件名
-export const ADD='add'
-export const MINUS='minus'
-export const ADD_PERSON='addPerson'
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// 引入所有reducer文件
-import {combineReducers} from '@reduxjs/toolkit'
-import countReducer from './count'
-import personReducer from './person'
-export default combineReducers({
-    num:countReducer,
-    people:personReducer
-})
